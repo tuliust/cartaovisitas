77bc3b480732644b63553f8d256b220c94c617ed
tuliust <tuliust@gmail.com>
2026-07-14 21:21:48 -0300

    style: load localized notebook responsive rules

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './logo-alignment.css'
 import './contact-card-overrides.css'
+import './notebook-responsive.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
