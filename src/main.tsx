fbc64738ae1ffb9b7bccdc72a1259c54eac1bdcd
tuliust <tuliust@gmail.com>
2026-07-14 21:18:55 -0300

    fix: remove adaptive density stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './logo-alignment.css'
 import './contact-card-overrides.css'
-import './responsive-density.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
