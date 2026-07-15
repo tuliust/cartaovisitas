27c9b4848b5645438f5c51bf0965d7d829fdd910
tuliust <tuliust@gmail.com>
2026-07-14 22:13:40 -0300

    style: load canonical desktop home scaling

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './logo-alignment.css'
 import './contact-card-overrides.css'
 import './notebook-responsive.css'
 import './home-notebook-overrides.css'
+import './home-desktop-scale.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
