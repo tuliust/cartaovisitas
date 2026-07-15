9eb27ecc226ca6152ff69a77eaaf0dc8db143634
tuliust <tuliust@gmail.com>
2026-07-14 21:50:24 -0300

    style: load final notebook home overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import './logo-alignment.css'
 import './contact-card-overrides.css'
 import './notebook-responsive.css'
+import './home-notebook-overrides.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
