9a67a7310d2f26bf174c79e0fde884a20cbf549b
tuliust <tuliust@gmail.com>
2026-07-14 21:09:44 -0300

    style: load adaptive responsive density

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './logo-alignment.css'
 import './contact-card-overrides.css'
+import './responsive-density.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
